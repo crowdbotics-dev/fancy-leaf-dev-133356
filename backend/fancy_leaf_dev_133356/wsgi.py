2b3280f906f71a7e34b79319fa65fad3ab0e9bf3
Crowdbotics <engineering@crowdbotics.com>
2024-02-13 16:12:16 +0000

    Initial commit

---
+++ backend/fancy_leaf_dev_133356/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for fancy_leaf_dev_133356 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE", "fancy_leaf_dev_133356.settings"
+)
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "fancy-leaf-dev-133356",
+  url: "https://fancy-leaf-dev-133356.botics.co",
+  api: "https://fancy-leaf-dev-133356.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
